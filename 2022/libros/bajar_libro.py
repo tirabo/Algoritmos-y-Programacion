cdc1474f7804cb67acebe32f0ee1bff389a175d7
Alejandro Tiraboschi <tirabo@gmail.com>
2022-06-04 12:55:47 -0300

    API Gutenberg

--- 2022/libros/bajar_libro.py
+++ 2022/libros/bajar_libro.py
@@ -5,7 +5,16 @@ from gutenberg.cleanup import strip_headers
 
 text = strip_headers(load_etext(2701)).strip()
 text = load_etext(2701)
-text = text.replace('\n', ' ')
+text = text.replace('\r\n', '\n')
+text = text.replace('\n\n', '\n')
 print(len(text))
 with open('moby_dick.txt', 'w', encoding="utf-8") as f:
+    f.write(text)
+
+text = strip_headers(load_etext(44112)).strip()
+text = load_etext(44112)
+text = text.replace('\r\n', '\n')
+text = text.replace('\n\n', '\n')
+print(len(text))
+with open('ARGENTINA_LEGEND_AND_HISTORY.txt', 'w', encoding="utf-8") as f:
     f.write(text)
