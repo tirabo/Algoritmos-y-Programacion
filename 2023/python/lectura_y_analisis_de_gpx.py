afea38b78e5c52d1bde943cb887aa1efaeada0a2
Alejandro Tiraboschi <tirabo@gmail.com>
2023-06-03 18:51:11 -0300

    Correcciones

--- 2023/python/lectura_y_analisis_de_gpx.py
+++ 2023/python/lectura_y_analisis_de_gpx.py
@@ -12,7 +12,7 @@ R = 6371000 # radio terrestre en metros
 PI = pi
 
 def distancia(punto1, punto2):
-    # pre:  punto1 y punto2 son coordenadas de dos puntos en radianes y su elevación en metros.
+    # pre:  punto1 = (lat1, lon1, ele1) y punto2 = (lat2, lon2, ele2) son coordenadas de dos puntos en radianes y su elevación en metros.
     # post: distancia entre los dos puntos en metros
     lat1, lon1, ele1 = punto1[0], punto1[1], punto1[2]
     lat2, lon2, ele2 = punto2[0], punto2[1], punto2[2]
@@ -71,24 +71,25 @@ def leer_gpx(nombre: str):
             cadence = np.append(cadence, np.nan)
         # FIN EXTENSIONES
     
-    lat_n, lon_n, ele_n = np.array([]), np.array([]), np.array([]) # datos obligatorios
-    time_n, temp_n, heart_rate_n, cadence_n = np.array([]), np.array([]), np.array([]), np.array([]) # datos opcionales (solo si es un recorrido realizado)
-    for i in range(len(lat)):
-        # if R*acos(cos(lat[i-1])*cos(lat[i])*cos(lon[i-1]-lon[i]) + sin(lat[i-1])*sin(lat[i])) != 0: # ELIMINAMOS PUNTOS A DISTANCIA PLANA = 0 
-        lat_n = np.append(lat_n, lat[i])
-        lon_n = np.append(lon_n, lon[i])
-        ele_n = np.append(ele_n, ele[i])
-    if ~np.isnan(time[0]):
-        for i in range(len(lat)):
-            time_n = np.append(time_n, time[i])
-            temp_n = np.append(temp_n, temp[i])
-            heart_rate_n = np.append(heart_rate_n, heart_rate[i])
-            cadence_n = np.append(cadence_n, cadence[i])
-    return  (lat_n, lon_n, ele_n, time_n, temp_n, heart_rate_n, cadence_n)
+    # lat_n, lon_n, ele_n = np.array([]), np.array([]), np.array([]) # datos obligatorios
+    # time_n, temp_n, heart_rate_n, cadence_n = np.array([]), np.array([]), np.array([]), np.array([]) # datos opcionales (solo si es un recorrido realizado)
+    # for i in range(len(lat)):
+    #     # if R*acos(cos(lat[i-1])*cos(lat[i])*cos(lon[i-1]-lon[i]) + sin(lat[i-1])*sin(lat[i])) != 0: # ELIMINAMOS PUNTOS A DISTANCIA PLANA = 0 
+    #     lat_n = np.append(lat_n, lat[i])
+    #     lon_n = np.append(lon_n, lon[i])
+    #     ele_n = np.append(ele_n, ele[i])
+    # if ~np.isnan(time[0]):
+    #     for i in range(len(lat)):
+    #         time_n = np.append(time_n, time[i])
+    #         temp_n = np.append(temp_n, temp[i])
+    #         heart_rate_n = np.append(heart_rate_n, heart_rate[i])
+    #         cadence_n = np.append(cadence_n, cadence[i])
+    # return  (lat_n, lon_n, ele_n, time_n, temp_n, heart_rate_n, cadence_n)
+    return  (lat, lon, ele, time, temp, heart_rate, cadence)
 
 
 def recorrido_proc(lat, lon, ele):
-    # pre: recorrido es una lista de ternas de (latitud, longitud, elevacion)
+    # pre: recorrido es una lista de ternas de (latitud, longitud, elevacion), cada una un arreglo de numnpy
     # post: devuelve una 5-upla (latitud, longitud, elevacion, distancia, pendiente) donde 
     #       - distancia en una coordenada es la distancia total desde el origen hasta ese punto
     #       - pendiente es el % de elevación entre ese punto y el anterior (la primera es 0.0)
@@ -106,6 +107,25 @@ def recorrido_proc(lat, lon, ele):
         pen = np.append(pen, pendiente)
     return lat, lon, ele, dis, pen
 
+"""
+def recorrido_proc(lat, lon, ele):
+    # pre: recorrido es una lista de ternas de (latitud, longitud, elevacion), cada una un arreglo de numnpy
+    # post: devuelve una 5-upla (latitud, longitud, elevacion, distancia, pendiente) donde 
+    #       - distancia en una coordenada es la distancia total desde el origen hasta ese punto
+    #       - pendiente es el % de elevación entre ese punto y el anterior (la primera es 0.0)
+    dis, pen = np.array([0]), np.array([0])
+    distancia_total = 0
+    datos = np.stack((lat, lon, ele), axis = 1) # cada coordenada es (lat1, lon1, ele1) un punto del recorrido
+    print(datos[0])
+    v_distancia = np.vectorize(distancia) # para poder aplicar distancia a dos arrays como map
+    distancias = np.array(v_distancia(datos[:-1], datos[1:])) # distancias entre cada punto y el siguiente
+    distancias = np.insert(distancias, 0.0, 0) # para que quede de la misma longitud que el original
+    v_pendiente = np.vectorize(pendiente) # para poder aplicar pendiente a dos arrays como map
+    pendientes = np.array(v_pendiente(datos[:-1], datos[1:])) # pendientes entre cada punto y el siguiente
+    pendientes = np.insert(pendientes, 0.0, 0) # para que quede de la misma longitud que el original
+    return lat, lon, ele, distancias, pendientes
+"""
+
 
 def declive_acumulado_positivo(elevacion):
     # pre: elevacion es un array numpy con los datos de altura en cada punto del recorrido (interpolados)
@@ -121,21 +141,19 @@ def velocidad_instantanea(distancia, tiempo):
     # pre: distancia es un array numpy con los datos de distancia que devuelve recorrido_proc()
     #      tiempo es un array numpy con los datos de tiempo que devuelve leer_gpx()
     # post: devuelve la velocidad instantánea en cada punto del recorrido
-    dif_dis, dif_time = np.diff(distancia), np.diff(tiempo)
-    diferencia_dist, diferencia_tiempo = dif_dis.copy(), dif_time.copy() 
-    diferencia_dist = np.insert(diferencia_dist, 0, 0) # agrega un 0 al principio
-    diferencia_tiempo = np.insert(diferencia_tiempo, 0, 1) # agrega un 1 al principio     
-    return 3.6 * diferencia_dist / diferencia_tiempo # en km/h
+    diferencia_dist, diferencia_tiempo = np.diff(distancia).copy(), np.diff(tiempo).copy() 
+    diferencia_dist = np.insert(diferencia_dist, 0, 0.0) # agrega un 0 al principio
+    diferencia_tiempo = np.insert(diferencia_tiempo, 0, 1.0) # agrega un 1 al principio
+    v_inst =  3.6 * diferencia_dist / diferencia_tiempo
+    return v_inst # en km/h
 
 
 def intervalos_movimiento(cadencia, velocidad):
     # pre: cadencia es un array numpy con los datos de cadencia que devuelve leer_gpx()
     #      velocidad es un array numpy con los datos de velocidad que devuelve velocidad_instantanea()
     # post: devuelve una ndarray con True si el punto es de movimiento y False si no lo es
-    #      (se considera que es de movimiento si la cadencia es mayor a 0 y la velocidad es mayor a 0)  
-    cadence = cadencia.copy() # para no modificar el array original
-    cadence[np.isnan(cadence)] = 0 # reemplaza los NaN por 0
-    mask = (cadence > 0) & (velocidad > 0)
+    #      (se considera que es de movimiento si la cadencia es mayor a 0 y la velocidad es mayor a 0.01)  
+    mask = ~np.isnan(cadencia) & ((cadencia > 0) | (velocidad > 1.0)) # True si hay movimiento, False si no lo es
     return mask 
 
 
@@ -150,21 +168,22 @@ def tiempo_en_movimiento(mask_mov, tiempo):
     return tiempo_mov
 
 
-def interpolar_dis_otro(distancia, otro):
-    # pre: distancia es un array numpy con los datos de distancia que devuelve recorrido_proc()
-    #      otro es un array numpy con los datos de elevacion o declive, etc, que devuelve recorrido_proc() o leer_gpx()
-    # post: devuelve una 2-upla (distancia, otro) con los datos interpolados
+def interpolar_s1_s2(serie_1, serie_2, cant_puntos = 1.2):
+    # pre: serie_1 es un array numpy con los datos de distancia o tiempo o etc. que devuelve recorrido_proc() o leer_gpx()
+    #      serie_2 es un array numpy con los datos de elevacion o declive, etc, que devuelve recorrido_proc() o leer_gpx()
+    #      cant_puntos es la proporción de puntos que se quieren agregar a los puntos originales (por defecto 1.2)
+    # post: devuelve una 2-upla (x_new, y_new) con los datos interpolados
     # https://docs.scipy.org/doc/scipy/tutorial/interpolate/smoothing_splines.html
     mask = np.array([True]) # máscara para saber qué puntos interpolamos 
-    # (todas las distancias deben ser distintas, si no, no interpola)
-    for i in range(1, len(distancia)):
-        if distancia[i] == distancia[i-1]:
+    # (todas los puntos de serie_1 deben ser distintas, si no, no interpola)
+    for i in range(1, len(serie_1)):
+        if serie_1[i] == serie_1[i-1]:
             mask = np.append(mask, False)
         else:
             mask = np.append(mask, True)
-    x, y = distancia[mask], otro[mask]
+    x, y = serie_1[mask], serie_2[mask]
     tck = interpolate.splrep(x, y, s = 0) # es mejor suavidad 0 
-    x_new = np.linspace(x.min(), x.max(), int(len(x) * 1.2)) # 20% más de puntos
+    x_new = np.linspace(x.min(), x.max(), int(len(x) * cant_puntos)) # 20% más de puntos
     y_new = interpolate.splev(x_new, tck, der=0)
     return x_new, y_new
 
@@ -181,29 +200,45 @@ def main():
     print('Número de puntos de la muestra:', nro_puntos)
 
     lat, lon, ele, dis, dec = recorrido_proc(latitud, longitud, elevacion)
-    print('Declive acumulado positivo:', int(declive_acumulado_positivo(ele_new)), 'metros.')
+    distancia_total = round(dis[-1]/1000,1) # distancia en km
+    print('Distancia total:', distancia_total, 'km.')
+    desnivel_positivo = int(declive_acumulado_positivo(ele)) # desnivel en metros
+    print('Declive acumulado positivo:', desnivel_positivo, 'metros.')
 
     # Dibujamos distancia vs elevación
-    plt.plot(dis, ele, '.', label='Datos originales')
-    dis_new, ele_new = interpolar_dis_otro(dis, ele)
-    plt.plot(dis_new, ele_new, label='Curva ajustada')
-    plt.legend()
-    plt.show()
+    # plt.plot(dis, ele, '.', label='Datos originales')
+    # dis_new, ele_new = interpolar_s1_s2(dis, ele)
+    # print('Declive acumulado positivo (suavizado):', int(declive_acumulado_positivo(ele_new)), 'metros.')
+    # plt.plot(dis_new, ele_new, label='Curva ajustada')
+    # plt.legend()
+    # plt.show()
     
     if len(time) > 0:
+        # Con 'Punilla_Observatorio.gpx' comparar con https://www.strava.com/activities/8573196220
         velocidad = velocidad_instantanea(dis, time)
+        # plt.plot(dis, velocidad, label='distancia / velocidad')
+        # plt.legend()
+        # plt.show()
         mask_mov = intervalos_movimiento(cadence, velocidad)
         print('La cadencia promedio en movimiento:', int(np.mean(cadence[mask_mov])), 'rpm.')
+        mask_mov_cad = mask_mov & (cadence > 0)
+        print('La cadencia promedio pedaleando:', int(np.mean(cadence[mask_mov_cad])), 'rpm.')
+        print('Cadencia máxima:', int(np.max(cadence[mask_mov])), 'rpm.')
         mask_ht = (~np.isnan(heart_rate))  # para sacar los NaN 
         print('La frecuencia cardiaca promedio:', int(np.mean(heart_rate[mask_ht])), 'bpm.')
-        velocidad_mov = velocidad[mask_mov]
-        print('La velocidad promedio (en movimiento):', round(np.mean(velocidad_mov),1), 'km/h.')  # ver si está bien
-        print('El tiempo total:', round((time[-1] - time[0]) / 3600,2), 'horas.')
-        print('El tiempo total en movimiento:',round(tiempo_en_movimiento(mask_mov, time) / 3600,2), 'horas.') # no está bien
+        print('La frecuencia cardiaca máxima:', int(np.max(heart_rate[mask_ht])), 'bpm.')
+        horas, minutos, segundos = int((time[-1] - time[0]) / 3600), int(((time[-1] - time[0]) % 3600) / 60), int(((time[-1] - time[0]) % 3600) % 60)
         # plt.plot(dis, heart_rate, label='Frecuencia cardiaca')
         # plt.plot(dis, cadence, label='Cadencia')
         # plt.legend()
         # plt.show()
+        print('El tiempo total: '+str(horas)+':'+str(minutos)+':'+str(segundos)+'.')
+        tiempo_mov = tiempo_en_movimiento(mask_mov, time)
+        horas_mov, minutos_mov, segundos_mov = int(tiempo_mov / 3600), int((tiempo_mov % 3600) / 60), int((tiempo_mov % 3600) % 60)
+        print('El tiempo total en movimiento: ' + str(horas_mov) + ':' + str(minutos_mov) + ':'+str(segundos_mov)) 
+        velocidad_promedio = round(distancia_total / (tiempo_mov / 3600), 1)
+        print('La velocidad promedio (en movimiento):', velocidad_promedio, 'km/h.') 
+        
         
 
 # Para ver métodos de suavizado: https://pythonguia.com/suavizado-python-scipy/
