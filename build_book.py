25fcac2d26495827aeab8f05195426062ff205d7
Alejandro Tiraboschi <tirabo@gmail.com>
2024-09-01 08:47:52 -0300

    mrnortes

--- build_book.py
+++ build_book.py
@@ -8,3 +8,4 @@ os.system('jupyter-book build book/')
 # Copia el contenido de _build/html a tirabo.github.io/ayp
 shutil.rmtree('../tirabo.github.io/ayp') # borra ../tirabo.github.io/ayp y su contenido 
 shutil.copytree('book/_build/html', '../tirabo.github.io/ayp') # crea el directorio ../tirabo.github.io/ayp y copia en él el contenido de _build/html
+os.system('ghp-import -n -p -f ../tirabo.github.io/ayp')
