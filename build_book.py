f65448deee3afead47c359a62bec6c0fc4764546
Alejandro Tiraboschi <tirabo@gmail.com>
2024-09-11 23:22:45 -0300

    Se agregan los practicos

--- build_book.py
+++ build_book.py
@@ -3,7 +3,7 @@ import os
 import shutil
 
 # Hace un build del Jupyter Book
-os.system('jupyter-book build book/')
+# os.system('jupyter-book build book/')
 
 # Copia el contenido de _build/html a tirabo.github.io/ayp
 shutil.rmtree('../tirabo.github.io/html/ayp') # borra ../tirabo.github.io/ayp y su contenido 
@@ -11,3 +11,4 @@ shutil.copytree('book/_build/html', '../tirabo.github.io/html/ayp') # crea el di
 os.chdir('../tirabo.github.io') # cambia al directorio ../tirabo.github.io
 os.system('ghp-import -n -p -f ./html') # hace un push de html a tirabo.github.io
 os.chdir('../Algoritmos-y-Programacion') # cambia al directorio actual
+print('Proceso terminado')
